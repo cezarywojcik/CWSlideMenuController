9dcb1ad657a4041714cfa44bea5934a303298cda
Cezary Wojcik <cezarywojcik92@gmail.com>
2013-09-20 10:19:57 -0700

    Initial Commit

---
+++ CWSlideMenuControllerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CWSlideMenuControllerDemo
+//
+//  Created by Cezary Wojcik on 9/20/13.
+//  Copyright (c) 2013 Cezary Wojcik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CWAppDelegate class]));
+    }
+}
